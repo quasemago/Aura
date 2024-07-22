8bb12c79ebc1036fe49ec677edc24f0b8305eeb7
quasemago <brunoronningfn@gmail.com>
2024-07-22 18:16:49 -0400

    feat(client): Create "ping" command, for testing purposes.

---
+++ src/main/java/dev/quasemago/aura/client/commands/impl/PingCommand.java
@@ -0,0 +1,41 @@
+package dev.quasemago.aura.client.commands.impl;
+
+import dev.quasemago.aura.client.commands.SlashCommand;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.discordjson.json.ApplicationCommandRequest;
+import discord4j.rest.util.Permission;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PingCommand implements SlashCommand {
+    @Override
+    public Mono<Void> execute(ChatInputInteractionEvent event) {
+        return event.reply("Pong!");
+    }
+
+    @Override
+    public String name() {
+        return "ping";
+    }
+
+    @Override
+    public String description() {
+        return "Ping Pong";
+    }
+
+    @Override
+    public Permission permission() {
+        return Permission.SEND_MESSAGES;
+    }
+
+    @Override
+    public ApplicationCommandRequest getCommand() {
+        return ApplicationCommandRequest.builder()
+                .name(name())
+                .description(description())
+                .dmPermission(false)
+                .defaultMemberPermissions(String.valueOf(permission().getValue()))
+                .build();
+    }
+}
