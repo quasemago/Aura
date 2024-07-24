ae98412a8bf7d264068efa6e96fcf11f853f8001
quasemago <brunoronningfn@gmail.com>
2024-07-24 16:45:55 -0400

    feat(client): Implements caching with redis in Jikan API searches.

---
+++ client/src/main/java/dev/quasemago/aura/client/config/RedisCacheConfig.java
@@ -0,0 +1,46 @@
+package dev.quasemago.aura.client.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        final var cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(24))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
--- client/src/main/java/dev/quasemago/aura/client/infra/searches/JikanSearchClient.java
+++ client/src/main/java/dev/quasemago/aura/client/infra/searches/JikanSearchClient.java
@@ -3,6 +3,7 @@ package dev.quasemago.aura.client.infra.searches;
 import dev.quasemago.aura.client.infra.searches.dto.AnimeSearchResponseDTO;
 import dev.quasemago.aura.client.infra.searches.dto.MalSearchResponseDTO;
 import dev.quasemago.aura.client.infra.searches.dto.MangaSearchResponseDTO;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,15 +15,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 )
 public interface JikanSearchClient {
     @GetMapping("/anime")
+    @Cacheable(value = "anime", key = "#title")
     AnimeSearchResponseDTO getAnimeByTitle(@RequestParam("q") String title,
                                            @RequestParam("limit") int limit,
                                            @RequestParam("sfw") boolean sfw);
 
     @GetMapping("/manga")
+    @Cacheable(value = "manga", key = "#title")
     MangaSearchResponseDTO getMangaByTitle(@RequestParam("q") String title,
                                            @RequestParam("limit") int limit,
                                            @RequestParam("sfw") boolean sfw);
 
     @GetMapping("/users/{username}/full")
+    @Cacheable(value = "mal-user", key = "#username")
     MalSearchResponseDTO getMalUserByName(@PathVariable("username") String username);
 }
